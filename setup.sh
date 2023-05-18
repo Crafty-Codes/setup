c73500caccf52e1aa0f73382f39eb008ea31dbae
Crafty-Codes <fabio.oberhofer@gmx.net>
2023-05-18 12:29:08 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -1,19 +1,4 @@
-yay -S \ 
-  extra/libwacom extra/xf86-input-wacom community/kcm-wacomtablet \ # wacom
-  extra/chromium aur/tor-browser \
-  aur/vscodium \
-  extra/krita extra/gimp \
-  community/spotify-launcher \ 
-  aur/mullvad-vpn \
-  community/kicad \
-  community/signal-desktop aur/threemawebqt-git \
-  extra/flatpak \
-  community/neovim \
-  community/qbittorrent \
-  aur/steam-devices-git \
-  aur/balena-etcher \
-  multilib/lib32-mesa extra/xf86-video-amdgpu extra/vulkan-radeon multilib/lib32-vulkan-radeon \ 
-  community/fish
+yay -S extra/libwacom extra/xf86-input-wacom community/kcm-wacomtablet extra/chromium aur/tor-browser aur/vscodium extra/krita extra/gimp community/spotify-launcher aur/mullvad-vpn community/kicad community/signal-desktop aur/threemawebqt-git extra/flatpak community/neovim community/qbittorrent aur/steam-devices-git aur/balena-etcher multilib/lib32-mesa extra/xf86-video-amdgpu extra/vulkan-radeon multilib/lib32-vulkan-radeon community/fish
 
 flatpak install net.lutris.Lutris com.valvesoftware.Steam net.davidotek.pupgui2
 
